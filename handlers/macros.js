114a255df7f811f75c18c6b8dd28f2de9fe3283f
SkyBeastMC <skybeastmc@gmail.com>
2017-09-03 17:55:15 +0200

    Voice macro fix

--- handlers/macros.js
+++ handlers/macros.js
@@ -26,22 +26,27 @@ class MacroHandler extends Handler
                 break;
         }
 
-        const lastChannel = this.app.client.voiceConnections.first().channel;
+        const lastConn    = this.app.client.voiceConnections.first();
+        const lastChannel = lastConn && lastConn.channel;
         const channel     = macro.audio && message.member.voiceChannel;
         if (channel)
         {
             this.app.voiceManager.connectionFor(channel)
-                .then(connection =>
-                      {
-                          const handler = connection.playFile(macro.audio);
-                          if (lastChannel)
-                          {
-                              handler.on('end', reason => lastChannel.join()
-                                  .then(connection => this.app.voiceManager.connect(connection))
-                                  .catch(err => {/* do nothing */})
-                              );
-                          }
-                      }
+                .then(connection => connection.playFile(macro.audio)
+                    .on('end', reason =>
+                        {
+                            if (lastChannel)
+                            {
+                                lastChannel.join()
+                                    .then(connection => this.app.voiceManager.connect(connection))
+                                    .catch(err => {/* do nothing */});
+                            }
+                            else
+                            {
+                                connection.disconnect();
+                            }
+                        }
+                    )
                 )
                 .catch(err => {/* do nothing */});
         }
